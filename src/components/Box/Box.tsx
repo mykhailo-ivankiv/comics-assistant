e8c75c4ab3fbfba45eccaca1103f65c414b074a8
Nina Bondar <bondar_n@ucu.edu.ua>
2021-03-20 21:27:10 +0200

    Move to react-three-fiber for component-like approach to three.js development

---
+++ src/components/Box/Box.tsx
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from "react";
+import { useFrame, Vector3 } from "react-three-fiber";
+
+interface Box {
+  position: Vector3 | undefined;
+}
+
+export const Box: React.FC<Box> = props => {
+  const mesh = useRef(null);
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame(() => {
+    if (mesh && mesh.current) {
+      // @ts-ignore
+      mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+    }
+  });
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={e => setActive(!active)}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}
+    >
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
--- src/components/SceneModelingSpace/SceneModelingSpace.tsx
+++ src/components/SceneModelingSpace/SceneModelingSpace.tsx
@@ -1,74 +1,15 @@
 import React from "react";
-import {
-  Scene,
-  Color,
-  Fog,
-  HemisphereLight,
-  WebGLRenderer,
-  PerspectiveCamera,
-  BoxGeometry,
-  MeshBasicMaterial,
-  Mesh
-} from "three";
+import { Canvas, useFrame } from "react-three-fiber";
+import { Box } from "../Box";
 
 const SceneModelingSpace = () => {
-  const scene = new Scene();
-  const camera = new PerspectiveCamera(
-    50,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+    </Canvas>
   );
-
-  React.useEffect(() => {
-    init();
-  }, []);
-
-  // @ts-ignore
-  const renderer = new WebGLRenderer({ antialias: true });
-  const hemiLight = new HemisphereLight(0xffffff, 0xffffff, 0.61);
-
-  const init = () => {
-    hemiLight.position.set(0, 50, 0);
-
-    scene.background = new Color(0xf1f1f1);
-    scene.fog = new Fog(0xf1f1f1, 60, 100);
-    scene.add(hemiLight);
-
-    camera.position.z = 30;
-    camera.position.x = 0;
-    camera.position.y = -3;
-
-    renderer.shadowMap.enabled = true;
-    renderer.setPixelRatio(window.devicePixelRatio);
-
-    var geometry = new BoxGeometry(1, 1, 1);
-    var material = new MeshBasicMaterial({ color: "000000" });
-    var cube = new Mesh(geometry, material);
-
-    scene.add(cube);
-    camera.position.z = 10;
-    camera.position.y = 0;
-
-    const animate = () => {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-    };
-
-    animate();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-  };
-
-  const update = () => {
-    renderer.render(scene, camera);
-    requestAnimationFrame(update);
-  };
-
-  return <div />;
 };
 
 export default SceneModelingSpace;
